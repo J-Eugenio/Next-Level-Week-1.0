873ab1c92168a81eae5d0ebb0494fd8ab98b96a2
Eugenio <eugeniopb4@gmail.com>
2020-06-07 12:25:06 -0300

    Parte Web Finalizada

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,16 @@
 import express from 'express'
+import cors from 'cors'
+import routes from './routes'
+import path from 'path'
 
 const app = express()
 
-app.get('/users', (request, response)=>{
-    console.log('Listagem de usuários')
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'José'
-    ])
-})
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
 
