52a75989c6dbc87c83fe67f18c1e1ec866dd5a11
Eugenio <eugeniopb4@gmail.com>
2020-06-07 21:04:30 -0300

    Implementação dos Selectes UF e City

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,124 +1,166 @@
-import React, { useState } from "react";
-import { Feather as Icon } from "@expo/vector-icons";
+import React, { useState, useEffect, ChangeEvent } from 'react'
+import axios from 'axios'
+import { Feather as Icon } from '@expo/vector-icons'
+import RNPickerSelect from 'react-native-picker-select'
 import {
   View,
   ImageBackground,
   StyleSheet,
   Image,
-  Text,
-  TextInput,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
+  Text
+} from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 
-import { RectButton } from "react-native-gesture-handler";
+interface IBGEUFResponse {
+  sigla: string
+}
 
-const Home: React.FC = () => {
-  const [uf, setUf] = useState("");
-  const [city, setCity] = useState("");
+interface IBGECityResponse {
+  nome: string
+}
+const Home = () => {
 
-  const navigation = useNavigation();
+  //Pega todas as cidades e UF selecionada
+  const [ufs, setUfs] = useState<string[]>([])
+  const [cities, setCities] = useState<string[]>([])
+
+  //Pega a cidade e a UF selecionada
+  const [selectedUf, setSelectedUf] = useState('0')
+  const [selectedCity, setSelectedCity] = useState('0')
+
+  //Navegar entre telas
+  const navigation = useNavigation()
+
+
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+      const ufInitials = response.data.map(uf => uf.sigla)
+      setUfs(ufInitials)
+    })
+  }, [])
+
+  useEffect(() => {
+    //Carrega as cidades sempre que a UF mudar
+    if (selectedUf === '0') {
+      return
+    }
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+      const cityNames = response.data.map(city => city.nome)
+      setCities(cityNames)
+    })
+  }, [selectedUf])
 
   function handleNavigateToPoints() {
-    navigation.navigate("Points", {
-      uf,
-      city,
-    });
+    //Navega para points e manda a cidade e a UF pra tela points
+    navigation.navigate('Points', {
+      uf: selectedUf,
+      city: selectedCity
+    })
   }
 
+  function handleSelectUf(value: string) {
+    ///função do select do UF
+    const uf = value
+    setSelectedUf(uf)
+  }
+  function handleSelectCity(value: string) {
+    //Função do select City
+    const city = value
+    setSelectedCity(city)
+  }
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    <ImageBackground
+      source={require('../../assets/home-background.png')}
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
     >
-      <ImageBackground
-        source={require("../../assets/home-background.png")}
-        style={styles.container}
-        imageStyle={{
-          width: 274,
-          height: 368,
-        }}
-      >
-        <View style={styles.main}>
-          <Image source={require("../../assets/logo.png")} />
-          <View>
-            <Text style={styles.title}>
-              Seu marketplace de coleta de resíduos
-            </Text>
-            <Text style={styles.description}>
-              Ajudamos pessoas a encontrarem pontos de coleta de forma
-              eficiente.
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RNPickerSelect
+          placeholder={{
+            label: 'Selecione o estado',
+            value: null,
+          }}
+          style={pickerSelectStyles}
+          onValueChange={(value) => handleSelectUf(value)}
+          items={ufs.map(uf => ({ label: uf, value: uf }))}
+        />
+        <RNPickerSelect
+          placeholder={{
+            label: 'Selecione cidade',
+            value: null,
+          }}
+          style={pickerSelectStyles}
+          onValueChange={(value) => handleSelectCity(value)}
+          items={cities.map(city => ({ label: city, value: city }))}
+        />
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
             </Text>
           </View>
-        </View>
-        <View style={styles.footer}>
-          <TextInput
-            style={styles.input}
-            value={uf}
-            onChangeText={setUf}
-            maxLength={2}
-            autoCapitalize="characters"
-            autoCorrect={false}
-            placeholder="Digite a UF"
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Digite a cidade"
-            value={city}
-            autoCorrect={false}
-            onChangeText={setCity}
-          />
-          <RectButton
-            style={styles.button}
-            onPress={() => handleNavigateToPoints()}
-          >
-            <View style={styles.buttonIcon}>
-              <Icon name="arrow-right" color="#fff" size={24} />
-            </View>
-            <Text style={styles.buttonText}>Entrar</Text>
-          </RectButton>
-        </View>
-      </ImageBackground>
-    </KeyboardAvoidingView>
-  );
-};
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  )
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
+
   },
 
   main: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
 
   title: {
-    color: "#322153",
+    color: '#322153',
     fontSize: 32,
-    fontFamily: "Ubuntu_700Bold",
+    fontFamily: 'Ubuntu_700Bold',
     maxWidth: 260,
     marginTop: 64,
   },
 
   description: {
-    color: "#6C6C80",
+    color: '#6C6C80',
     fontSize: 16,
     marginTop: 16,
-    fontFamily: "Roboto_400Regular",
+    fontFamily: 'Roboto_400Regular',
     maxWidth: 260,
     lineHeight: 24,
   },
 
   footer: {},
 
-  select: {},
+  select: {
+    fontSize: 16,
+    paddingTop: 13,
+    paddingHorizontal: 10,
+    paddingBottom: 12,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    backgroundColor: 'white',
+    color: 'black',
+  },
 
   input: {
     height: 60,
-    backgroundColor: "#FFF",
+    backgroundColor: '#FFF',
     borderRadius: 10,
     marginBottom: 8,
     paddingHorizontal: 24,
@@ -126,31 +168,44 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    backgroundColor: "#34CB79",
+    backgroundColor: '#34CB79',
     height: 60,
-    flexDirection: "row",
+    flexDirection: 'row',
     borderRadius: 10,
-    overflow: "hidden",
-    alignItems: "center",
+    overflow: 'hidden',
+    alignItems: 'center',
     marginTop: 8,
   },
 
   buttonIcon: {
     height: 60,
     width: 60,
-    backgroundColor: "rgba(0, 0, 0, 0.1)",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
   buttonText: {
     flex: 1,
-    justifyContent: "center",
-    textAlign: "center",
-    color: "#FFF",
-    fontFamily: "Roboto_500Medium",
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
     fontSize: 16,
+  }
+});
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingTop: 13,
+    paddingHorizontal: 10,
+    paddingBottom: 12,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    backgroundColor: 'white',
+    color: 'black',
+    marginBottom: 5
   },
 });
-
-export default Home;
+export default Home
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { useNavigation, useRoute } from "@react-navigation/native";
-
+import React, { useState, useEffect } from 'react'
 import {
   View,
   StyleSheet,
@@ -8,157 +6,140 @@ import {
   Text,
   ScrollView,
   Image,
-  Alert,
-} from "react-native";
-
-import { Feather as Icon } from "@expo/vector-icons";
-import Constants from "expo-constants";
-import MapView, { Marker } from "react-native-maps";
-import { SvgUri } from "react-native-svg";
-import api from "../../services/api";
-
-import * as Location from "expo-location";
+  SafeAreaView,
+  Alert
+} from 'react-native'
+import { Feather as Icon } from '@expo/vector-icons'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import Constants from 'expo-constants'
+import * as Location from 'expo-location'
+import MapView, { Marker } from 'react-native-maps'
+import { SvgUri } from 'react-native-svg'
+import api from '../../services/api'
 
 interface Item {
-  id: number;
-  title: string;
-  image_url: string;
+  id: number
+  title: string
+  image_url: string
 }
 
 interface Point {
-  id: number;
-  name: string;
-  latitude: number;
-  longitude: number;
-  image: string;
+  id: number
+  name: string
+  image: string
+  latitude: number
+  longitude: number
 }
 
-interface Params {
-  uf: string;
-  city: string;
+interface Params{
+  uf: string
+  city: string
 }
 
-const Points: React.FC = () => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [points, setPoints] = useState<Point[]>([]);
-  const [selectedItems, setSelectedItems] = useState<number[]>([]);
-  const [initialPosition, setInitialPosition] = useState<[number, number]>([
-    0,
-    0,
-  ]);
-  const navigation = useNavigation();
-  const route = useRoute();
+const Points = () => {
+  const [items, setItems] = useState<Item[]>([])
+  const [points, setPoints] = useState<Point[]>([])
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
 
-  const routeParams = route.params as Params;
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
 
-  useEffect(() => {
-    async function loadPoints() {
-      const response = await api.get("/items");
+  const route = useRoute()
+  const routeParams = route.params as Params
 
-      setItems(response.data);
-    }
-
-    loadPoints();
-  }, []);
+  const navigation = useNavigation()
 
   useEffect(() => {
     async function loadPosition() {
-      const { status } = await Location.requestPermissionsAsync();
+      const { status } = await Location.requestPermissionsAsync()
 
-      if (status !== "granted") {
-        Alert.alert(
-          "Ooooops...",
-          "Precisamos de sua permissão para obter a localização"
-        );
-        return;
+      if (status !== 'granted') {
+        Alert.alert('Oooops....', 'Precisamos de sua permissão para obter a localização')
+        return
       }
 
-      const location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true })
+      const location = await Location.getCurrentPositionAsync()
+      const { latitude, longitude } = location.coords
 
-      const { latitude, longitude } = location.coords;
-
-      setInitialPosition([latitude, longitude]);
+      setInitialPosition([
+        latitude,
+        longitude
+      ])
     }
 
-    loadPosition();
-  });
+    loadPosition()
+  }, [])
 
   useEffect(() => {
-    async function loadPoints() {
-      const response = await api.get("/points", {
-        params: {
-          city: routeParams.city,
-          uf: routeParams.uf,
-          items: selectedItems,
-        },
-      });
+    api.get('items').then(response => {
+      setItems(response.data)
+    })
+  }, [])
 
-      setPoints(response.data);
-    }
-
-    loadPoints();
-  }, [selectedItems]);
+  useEffect(() => {
+    api.get('points', {
+      params: {
+        city: routeParams.city,
+        uf: routeParams.uf,
+        items:selectedItems
+      }
+    }).then(response => {
+      setPoints(response.data)
+    })
+  }, [selectedItems])
 
   function handleNavigateBack() {
-    navigation.goBack();
+    navigation.goBack()
   }
 
   function handleNavigateToDetail(id: number) {
-    navigation.navigate("Detail", { point_id: id });
+    navigation.navigate('Detail', {point_id: id})
   }
 
   function handleSelectItem(id: number) {
-    const alreadySelected = selectedItems.findIndex((item) => item === id);
-
+    const alreadySelected = selectedItems.findIndex(item => item === id)
     if (alreadySelected >= 0) {
-      const filteredItems = selectedItems.filter((item) => item !== id);
-
-      setSelectedItems(filteredItems);
+      const filteredItems = selectedItems.filter(item => item !== id)
+      setSelectedItems(filteredItems)
     } else {
-      setSelectedItems([...selectedItems, id]);
+      setSelectedItems([...selectedItems, id])
     }
+
   }
 
   return (
-    <>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
         <TouchableOpacity onPress={handleNavigateBack}>
           <Icon name="arrow-left" size={20} color="#34cb79" />
         </TouchableOpacity>
 
         <Text style={styles.title}>Bem vindo.</Text>
-        <Text style={styles.description}>
-          Encontre no mapa um ponto de coleta.
-        </Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
 
         <View style={styles.mapContainer}>
           {initialPosition[0] !== 0 && (
             <MapView
               style={styles.map}
+              loadingEnabled={initialPosition[0] === 0}
               initialRegion={{
                 latitude: initialPosition[0],
                 longitude: initialPosition[1],
-                latitudeDelta: 0.014,
-                longitudeDelta: 0.014,
+                latitudeDelta: 0.010,
+                longitudeDelta: 0.010
               }}
             >
-              {points.map((point) => (
+              {points.map(point => (
                 <Marker
                   key={String(point.id)}
                   style={styles.mapMarker}
-                  onPress={() => handleNavigateToDetail(point.id)}
+                  onPress={()=>{handleNavigateToDetail(point.id)}}
                   coordinate={{
                     latitude: point.latitude,
-                    longitude: point.longitude,
+                    longitude: point.longitude
                   }}
                 >
                   <View style={styles.mapMarkerContainer}>
-                    <Image
-                      style={styles.mapMarkerImage}
-                      source={{
-                        uri: point.image,
-                      }}
-                    />
+                    <Image style={styles.mapMarkerImage} source={{ uri: point.image }} />
                     <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                   </View>
                 </Marker>
@@ -169,31 +150,32 @@ const Points: React.FC = () => {
       </View>
       <View style={styles.itemsContainer}>
         <ScrollView
-          contentContainerStyle={{
-            paddingHorizontal: 32,
-          }}
           horizontal
           showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
         >
-          {items.map((item) => (
+          {items.map(item => (
             <TouchableOpacity
               key={String(item.id)}
               style={[
                 styles.item,
-                selectedItems.includes(item.id) ? styles.selectedItem : {},
+                selectedItems.includes(item.id) ? styles.selectedItem : {}
               ]}
-              onPress={() => handleSelectItem(item.id)}
-              activeOpacity={0.6}
+              onPress={() => { handleSelectItem(item.id) }}
+              activeOpacity={.6}
             >
               <SvgUri width={42} height={42} uri={item.image_url} />
               <Text style={styles.itemTitle}>{item.title}</Text>
             </TouchableOpacity>
           ))}
+
         </ScrollView>
       </View>
-    </>
-  );
-};
+    </SafeAreaView>
+  )
+}
+
+export default Points
 
 const styles = StyleSheet.create({
   container: {
@@ -204,28 +186,28 @@ const styles = StyleSheet.create({
 
   title: {
     fontSize: 20,
-    fontFamily: "Ubuntu_700Bold",
+    fontFamily: 'Ubuntu_700Bold',
     marginTop: 24,
   },
 
   description: {
-    color: "#6C6C80",
+    color: '#6C6C80',
     fontSize: 16,
     marginTop: 4,
-    fontFamily: "Roboto_400Regular",
+    fontFamily: 'Roboto_400Regular',
   },
 
   mapContainer: {
     flex: 1,
-    width: "100%",
+    width: '100%',
     borderRadius: 10,
-    overflow: "hidden",
+    overflow: 'hidden',
     marginTop: 16,
   },
 
   map: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
   },
 
   mapMarker: {
@@ -236,37 +218,37 @@ const styles = StyleSheet.create({
   mapMarkerContainer: {
     width: 90,
     height: 70,
-    backgroundColor: "#34CB79",
-    flexDirection: "column",
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
     borderRadius: 8,
-    overflow: "hidden",
-    alignItems: "center",
+    overflow: 'hidden',
+    alignItems: 'center'
   },
 
   mapMarkerImage: {
     width: 90,
     height: 45,
-    resizeMode: "cover",
+    resizeMode: 'cover',
   },
 
   mapMarkerTitle: {
     flex: 1,
-    fontFamily: "Roboto_400Regular",
-    color: "#FFF",
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
     fontSize: 13,
     lineHeight: 23,
   },
 
   itemsContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginTop: 16,
     marginBottom: 32,
   },
 
   item: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     borderWidth: 2,
-    borderColor: "#eee",
+    borderColor: '#eee',
     height: 120,
     width: 120,
     borderRadius: 8,
@@ -274,22 +256,20 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     paddingBottom: 16,
     marginRight: 8,
-    alignItems: "center",
-    justifyContent: "space-between",
+    alignItems: 'center',
+    justifyContent: 'space-between',
 
-    textAlign: "center",
+    textAlign: 'center',
   },
 
   selectedItem: {
-    borderColor: "#34CB79",
+    borderColor: '#34CB79',
     borderWidth: 2,
   },
 
   itemTitle: {
-    fontFamily: "Roboto_400Regular",
-    textAlign: "center",
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
     fontSize: 13,
   },
 });
-
-export default Points;
